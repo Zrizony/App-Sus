152f1ccfd99b47c45e9142ceb144bd3a2941a60b
Roy Yam <94809737+Zrizony@users.noreply.github.com>
2022-08-24 16:30:05 +0300

    updated notes views
    updated notes views

--- apps/note/views/note-app.jsx
+++ apps/note/views/note-app.jsx
@@ -1,9 +1,90 @@
+import { eventBusService } from '../../../services/event-bus.service.js'
+import { noteService } from '../services/note.service.js'
+import { NoteList } from '../cmps/note-list.jsx'
+import { NoteAdd } from '../cmps/note-add.jsx'
+import { Screen } from '../cmps/screen.jsx'
 
 export class NoteApp extends React.Component {
-
-    render() {
-        return (
-            <div>Hello From NoteApp!</div>
-        )
+    state = {
+      filterBy: '',
+      notes: [],
+      selectedNote: null,
     }
-}
+  
+    componentDidMount() {
+      let searchQuery = new URLSearchParams(this.props.location.search).get(
+        'search'
+      )
+      if (!searchQuery) this.loadNotes()
+      this.setState(
+        (prevState) => ({ ...prevState, filterBy: searchQuery }),
+        this.loadNotes
+      )
+    }
+  
+    componentDidUpdate(prevProps, prevState) {
+      let searchQuery = new URLSearchParams(this.props.location.search).get(
+        'search'
+      )
+      let prevSearch = new URLSearchParams(prevProps.location.search).get(
+        'search'
+      )
+      if (prevSearch !== searchQuery) {
+        this.setState(
+          (prevState) => ({ ...prevState, filterBy: searchQuery }),
+          this.loadNotes
+        )
+      }
+    }
+  
+    onSelectNote = (noteId) => {
+      eventBusService.emit('screen', true)
+      this.setState((prevState) => ({ ...prevState, selectedNote: noteId }))
+    }
+  
+    onPinNote = (ev, noteId) => {
+      ev.preventDefault()
+      noteService
+        .togglePin(noteId)
+        .then((notes) => this.setState((prevState) => ({ ...prevState, notes })))
+    }
+  
+    onDeleteNote = (noteId) => {
+      noteService
+        .deleteNote(noteId)
+        .then((notes) => this.setState((prevState) => ({ ...prevState, notes })))
+    }
+  
+    onDuplicateNote = (noteId) => {
+      noteService
+        .duplicateNote(noteId)
+        .then((notes) => this.setState((prevState) => ({ ...prevState, notes })))
+    }
+  
+    loadNotes = () => {
+      const { filterBy } = this.state
+      noteService.query(filterBy).then((notes) => {
+        this.setState((prevState) => ({ ...prevState, notes }))
+      })
+    }
+  
+    render() {
+      const { notes } = this.state
+      return (
+        <React.Fragment>
+          <section className='note-app'>
+            <NoteAdd loadNotes={this.loadNotes} />
+            <NoteList
+              notes={notes}
+              onDeleteNote={this.onDeleteNote}
+              onDuplicateNote={this.onDuplicateNote}
+              onPinNote={this.onPinNote}
+            />
+  
+            <Screen />
+          </section>
+        </React.Fragment>
+      )
+    }
+  }
+  
