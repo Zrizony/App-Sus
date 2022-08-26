1d7fe4ca6865bb0a66ea7039ac1e8f804fd79df9
Zrizony <royyam99@gmail.com>
2022-08-26 21:24:44 +0300

    fixed home imgs

--- views/home.jsx
+++ views/home.jsx
@@ -4,15 +4,15 @@ export function Home() {
       <h1>Welcome to home page!</h1>
       <section className="home-apps">
         <article>
-          <img src="../assets/img/notes.png" alt="notes" />
+          <img src="./assets/img/notes.png" alt="notes" />
           <h2>notes</h2>
         </article>
         <article>
-          <img src="../assets/img/email.png" alt="email" />
+          <img src="./assets/img/email.png" alt="email" />
           <h2>email</h2>
         </article>
         <article>
-          <img src="../assets/img/book.png" alt="book" />
+          <img src="./assets/img/book.png" alt="book" />
           <h2>books</h2>
         </article>
       </section>
